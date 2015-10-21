746cd7e263389ced1baae1a3751c4adc4f6d9153
chexee <chelsea@vouch.com>
2015-10-21 21:16:05 +0200

    Use CSS class swapping to animate

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,10 +2,7 @@ var animatePoints = function() {
     var points = document.getElementsByClassName('point');
 
     var revealPoint = function(point) {
-        point.style.opacity = 1;
-        point.style.transform = "scaleX(1) translateY(0)";
-        point.style.msTransform = "scaleX(1) translateY(0)";
-        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+        point.addClass('point-animate-in-finish');
     }
 
     for (var i = points.length - 1; i >= 0; i--) {
