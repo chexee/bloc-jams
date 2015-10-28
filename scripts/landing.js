b0b0e538f8e6179be20a1a1c1758dd1e13e4e29c
chexee <chelsea@vouch.com>
2015-10-27 17:08:51 -0700

    appear as the user scrolls, fix addClass bug
    Hmm, I thought I tested using addClass outside of jQuery, but I guess
    it was caching? Anyway, setAttribute works :P

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,21 +2,23 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
   var revealPoint = function(point) {
-    point.addClass('point-animate-in-finish');
+    point.setAttribute('class', 'point point-animate-in-finish');
   }
 
   for (var i = points.length - 1; i >= 0; i--) {
     revealPoint(points[i]);
   };
-
 };
 
 
 window.onload = function() {
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
   window.addEventListener('scroll', function(points) {
-      console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+    if (document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
   });
 
 }
