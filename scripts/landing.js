f83e41ff057fe52937e6b8686f9085ad1640ecf0
chexee <chelsea@vouch.com>
2015-10-28 09:32:49 -0700

    Add utilities JS

--- scripts/landing.js
+++ scripts/landing.js
@@ -17,7 +17,6 @@ window.onload = function() {
     animatePoints(pointsArray);
   }
 
-
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
