977a0ccec2a7e7243fb58303ffc5962c79aad4ed
chexee <chelsea@vouch.com>
2015-10-27 17:03:33 -0700

    Show current offset in console, fix spacing

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,22 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-    var revealPoint = function(point) {
-        point.addClass('point-animate-in-finish');
-    }
+var animatePoints = function(points) {
+  var revealPoint = function(point) {
+    point.addClass('point-animate-in-finish');
+  }
 
-    for (var i = points.length - 1; i >= 0; i--) {
-        revealPoint(points[i]);
-    };
+  for (var i = points.length - 1; i >= 0; i--) {
+    revealPoint(points[i]);
+  };
 
 };
+
+
+window.onload = function() {
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+
+  window.addEventListener('scroll', function(points) {
+      console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+  });
+
+}
