f65723fd248e0f66c89ac44b4968ea5efb71ad3e
chexee <chelsea@vouch.com>
2015-10-29 09:51:09 -0700

    Create rows from objects, remove static HTML

--- scripts/album.js
+++ scripts/album.js
@@ -5,7 +5,7 @@ var albumPicasso = {
   artist: 'Pablo Picasso',
   label: 'Cubism',
   year: '1881',
-  albumArtUrl: 'assets/images/album_covers/01.png'
+  albumArtUrl: 'assets/images/album_covers/01.png',
   songs: [
     { name: 'Blue',     length: '4:26' },
     { name: 'Green',    length: '3:14' },
@@ -29,3 +29,38 @@ var albumMarconi = {
     { name: 'Wrong phone number',   length: '2:15'  }
   ]
 };
+
+var createSongRow = function(songNumber, songName, songLength) {
+  var template =
+      '<tr class="album-view-song-item">'
+    + '  <td class="song-item-number">' + songNumber + '</td>'
+    + '  <td class="song-item-title">' + songName + '</td>'
+    + '  <td class="song-item-duration">' + songLength + '</td>'
+    + '</tr>'
+    ;
+
+  return template;
+}
+
+var setCurrentAlbum = function(album) {
+  var albumTitle = document.getElementsByClassName('album-view-title')[0];
+  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+  var albumImage = document.getElementsByClassName('album-cover-art')[0];
+  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+  albumTitle.firstChild.nodeValue = album.name;
+  albumArtist.firstChild.nodeValue = album.artist;
+  albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+  albumImage.setAttribute('src', album.albumArtUrl);
+
+  albumSongList.innerHTML = '';
+
+  for (var i = 0; i < album.songs.length; i++) {
+    albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+  };
+};
+
+ window.onload = function() {
+  setCurrentAlbum(albumPicasso);
+ };
